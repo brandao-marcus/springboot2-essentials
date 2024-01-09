4313642ae82154d4b7eca6241e1e9663b5d86b66
Marcus Brandão <marcusergue@gmail.com>
2024-01-09 10:40:33 -0300

    update aulas 09/01/24

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -29,7 +29,7 @@ public class AnimeController {
     @GetMapping(path = "list2")
     public List<Anime> list2() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Dragon Ball Z"), new Anime("Sakura Card Captors"));
+        return List.of(new Anime("Dragon Ball Z"), new Anime("Sakura Card Captors"), new Anime("YuYu Hakusho"));
     }
 
     @GetMapping(path = "list3")
